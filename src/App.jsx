63dfd22abaadfa584fca804ff9c070b4d3820b30
1ravi48 <ravi48@hotmail.co.uk>
2025-03-08 19:43:48 +0000

    hover feature on sorting hat image integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 import HouseColourBackground from "./components/HouseColourBackground/HouseColourBackground";
+import { useState } from "react";
 
 export default function App() {
+  const [isHover, setIsHover] = useState(false);
+
+  function handleHover() {
+    setIsHover(true);
+  }
+
+  function handleNotHover() {
+    setIsHover(false);
+  }
+
   return (
     <div className="page">
       <div className="four-colours">
@@ -10,7 +21,15 @@ export default function App() {
         <HouseColourBackground backgroundColor="#222f5b" />
         <HouseColourBackground backgroundColor="#ecb939" />
       </div>
-      <img src="../public/sorting-hat.svg" className="sorting-hat" />
+      <img
+        src="../public/hogwarts-sorting-hat.png"
+        className={isHover ? "sorting-hat-enlarged" : "sorting-hat"}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleNotHover}
+      />
+      <p className={isHover ? "sorting-hat-greet" : "sorting-hat-not-greet"}>
+        Click me to begin the sorting ceremony
+      </p>
     </div>
   );
 }
