cecc682d252b3c01734c516e9b2ef4ddfa08b857
1ravi48 <ravi48@hotmail.co.uk>
2025-03-08 17:45:14 +0000

    house colours set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
+import HouseColourBackground from "./components/HouseColourBackground/HouseColourBackground";
 
 export default function App() {
-  return <p>Hello World</p>;
+  return (
+    <div className="page">
+      <div className="four-colours">
+        <HouseColourBackground backgroundColor="#ae0001" />
+        <HouseColourBackground backgroundColor="#1a472a" />
+        <HouseColourBackground backgroundColor="#222f5b" />
+        <HouseColourBackground backgroundColor="#ecb939" />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/HouseColourBackground/HouseColourBackground.jsx
@@ -0,0 +1,5 @@
+import "./HouseColourBackground.css";
+
+export default function HouseColourBackground({backgroundColor}) {
+  return <div style={{backgroundColor: backgroundColor}} className="house-background"></div>;
+}
