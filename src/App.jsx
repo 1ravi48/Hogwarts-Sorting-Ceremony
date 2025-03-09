613a2be558627a4ae79526adfe0f0ef8337b4e58
1ravi48 <ravi48@hotmail.co.uk>
2025-03-09 16:03:04 +0000

    question page set up partially done. Question state variable implemented and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import HouseColourBackground from "./components/HouseColourBackground/HouseColourBackground";
 import { useState } from "react";
+import AnswerChoice from "./components/AnswerChoice/AnswerChoice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackward } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   const [isHover, setIsHover] = useState(false);
+  const [question, setQuestion] = useState(0);
+  const [isBackHover, setIsBackHover] = useState(false);
 
   function handleHover() {
     setIsHover(true);
@@ -13,39 +18,78 @@ export default function App() {
     setIsHover(false);
   }
 
+  function handleClick() {
+    setQuestion(question + 1);
+  }
+
+  function handleBackHover() {
+    setIsBackHover(true);
+  }
+
+  function handleBackLeave() {
+    setIsBackHover(false);
+  }
+
+  function handleBackClick() {
+    setQuestion(question - 1);
+  }
+
   return (
     <div className="page">
-      <div className="four-colours">
-        <HouseColourBackground
-          backgroundColor="#95103b"
-          imagePath="../public/gryffindor-logo.png"
-        />
-        <HouseColourBackground
-          backgroundColor="#1d7452"
-          imagePath="../public/slytherin-logo.png"
-        />
-        <HouseColourBackground
-          backgroundColor="#97b6d3"
-          imagePath="../public/ravenclaw-logo.png"
-        />
-        <HouseColourBackground
-          backgroundColor="#ffebb9"
-          imagePath="../public/hufflepuff-logo.png"
-        />
-        <img
-          src="../public/hogwarts-sorting-hat.png"
-          className={isHover ? "sorting-hat-enlarged" : "sorting-hat"}
-          onMouseEnter={handleHover}
-          onMouseLeave={handleNotHover}
-        />
-      </div>
-      <p
-        className={isHover ? "sorting-hat-greet" : "sorting-hat-not-greet"}
-        onMouseEnter={handleHover}
-        onMouseLeave={handleNotHover}
-      >
-        Let's begin the <br /> sorting ceremony...
-      </p>
+      {question === 0 ? (
+        <div className="four-colours">
+          <HouseColourBackground
+            backgroundColor="#95103b"
+            imagePath="../public/gryffindor-logo.png"
+          />
+          <HouseColourBackground
+            backgroundColor="#1d7452"
+            imagePath="../public/slytherin-logo.png"
+          />
+          <HouseColourBackground
+            backgroundColor="#97b6d3"
+            imagePath="../public/ravenclaw-logo.png"
+          />
+          <HouseColourBackground
+            backgroundColor="#ffebb9"
+            imagePath="../public/hufflepuff-logo.png"
+          />
+          <img
+            src="../public/hogwarts-sorting-hat.png"
+            className={isHover ? "sorting-hat-enlarged" : "sorting-hat"}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleNotHover}
+            onClick={handleClick}
+          />
+          <p
+            className={isHover ? "sorting-hat-greet" : "sorting-hat-not-greet"}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleNotHover}
+            onClick={handleClick}
+          >
+            Let's begin the <br /> sorting ceremony...
+          </p>
+        </div>
+      ) : (
+        <div className="answer-choices">
+          <AnswerChoice />
+          <AnswerChoice />
+          <AnswerChoice />
+          <AnswerChoice />
+          <img src="/parchment.png" className="parchment"></img>
+          <h1 className="question-heading">Question 1</h1>
+          <p className="question-text">
+            Here is an example of a question. What is your favourite colour?
+          </p>
+          <FontAwesomeIcon
+            icon={faBackward}
+            className={isBackHover ? "backward-icon-enlarged" : "backward-icon"}
+            onMouseEnter={handleBackHover}
+            onMouseLeave={handleBackLeave}
+            onClick={handleBackClick}
+          />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/AnswerChoice/AnswerChoice.jsx
@@ -0,0 +1,13 @@
+import "./AnswerChoice.css"
+
+export default function AnswerChoice() {
+    return (
+        <div className="answer-choice-div">
+        <p
+          className="answer-choice-text"
+        >
+            Answer choice
+        </p>
+        </div>
+      );
+}
